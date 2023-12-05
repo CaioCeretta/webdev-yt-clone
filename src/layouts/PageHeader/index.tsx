82c71e316051a43959e7a5b2edaaf650f9723c82
Caio Ceretta <caioceretta@gmail.com>
2023-12-05 09:22:32 -0300

    .

--- src/layouts/PageHeader/index.tsx
+++ src/layouts/PageHeader/index.tsx
@@ -1,19 +1,44 @@
-import { Menu } from 'lucide-react'
+import { Bell, Menu, Mic, Search, Upload, User } from 'lucide-react'
 import logo from '../../assets/Logo.png'
 import Button from '../../components/Button'
 
 export function PageHeader() {
   return (
-    <div className="flex flex-row gap-10 lg:gap-20 justify-between">
+    <div className="flex flex-row gap-10 lg:gap-20 pt-2 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <Button variant={'ghost'} size={'icon'} className='p-10'>
-          <Menu /> 
+        <Button variant={'ghost'} size={'icon'}>
+          <Menu />
         </Button>
         <a href="/">
-          <img src={logo} alt="Logo" className='h-10' />
+          <img src={logo} alt="Logo" className='h-6' />
         </a>
       </div>
-  
+      <div>
+        <form className='flex gap-4 flex-grow justify-center'>
+          <div className='flex flex-grow max-w-[600px]'>
+            <input type='search' placeholder='Search' className='rounded-l-full border border-secondary-border py-1 px-4 text-lg w-full focus:border-blue-500 outline-none' />
+            <Button className='py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0'>
+              <Search />
+            </Button>
+          </div>
+          <Button type='button' size='icon' className='flex-shrink-0'>
+            <Mic />
+          </Button>
+        </form>
+      </div>
+      <div className='flex flex-shrink-0 md:gap-2 lg:gap-4'>
+        <Button size='icon' variant='ghost'>
+          <User />
+        </Button>
+        <Button size='icon' variant='ghost'>
+          <Bell />
+        </Button>
+        <Button size='icon' variant='ghost'>
+          <Upload />
+        </Button>
+
+      </div>
+
     </div>
   )
 }
