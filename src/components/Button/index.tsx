d2b7a55321abef24c12c8f91904186f48f7678ba
Caio Ceretta <caioceretta@gmail.com>
2023-12-05 23:52:12 -0300

    .

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,11 +6,12 @@ const buttonStyles = cva(["", "transition-colors"], {
   variants: {
     variant: {
       default: ['bg-secondary', 'hover:bg-secondary-hover'],
-      ghost: ['bg-gray-100']
+      ghost: ['bg-gray-100'],
+      dark: ['bg-secondary-dark', 'hover:bg-secondary-dark-hover', 'text-secondary']
     },
     size: {
       default: ['rounded', 'p-2'],
-      icon: ['rounded-full', 'w-10', 'h-10', 'flex', 'items-center', 'justify-center', 'p-2.5'],
+      icon: ['rounded-full', 'w-10', 'h-10', 'flex', 'items-center', 'justify-center', 'p-2.5', 'mx-2' ],
     },
   },
   defaultVariants: {
@@ -18,7 +19,7 @@ const buttonStyles = cva(["", "transition-colors"], {
     size: 'default'
   }
 });
-
+ 
 type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<'button'>
 
 /*twMerge resolve conflicts on the default classes and the classes that are passed through the props making sure
